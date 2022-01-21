93927c455a76aa938a58465d16cf3f7e832cfe00
Mani1596 <rockpotmani1596@gmail.com>
2022-01-21 12:37:59 -0800

    project on 21-01-22

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,23 @@
+package org.add;
+
+public class GreensTech {
+private void greensOmr() {
+System.out.println("Greenstech in OMR");
+}
+private void greensVelacherry() {
+System.out.println("Greenstech in Velacherry");
+}
+private void greensTambaram() {
+System.out.println("Greenstech in Tambaram");
+}
+private void greensAdyar() {
+System.out.println("Greenstech in Adyar");
+}
+public static void main(String[] args) {
+	GreensTech g = new GreensTech();
+			g.greensOmr();
+	g.greensVelacherry();
+	g.greensTambaram();
+	g.greensAdyar();
+}
+}
